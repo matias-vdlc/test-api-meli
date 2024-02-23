dce77e37306c5c4097426efe860d09b4c68f6c40
matias-vdlc <matias.vdlc@gmail.com>
2024-02-22 21:16:31 -0300

    Fix format.

--- helpers/searchService.js
+++ helpers/searchService.js
@@ -10,12 +10,16 @@ function findMostCommonCategory(items) {
     counts[item.category_id] = (counts[item.category_id] || 0) + 1;
     return counts;
   }, {});
-  return Object.keys(categoryCounts).reduce((a, b) => categoryCounts[a] > categoryCounts[b] ? a : b);
+  return Object.keys(categoryCounts).reduce((a, b) =>
+    categoryCounts[a] > categoryCounts[b] ? a : b
+  );
 }
 
 function getCategoryPath(filters, mostCommonCategoryId) {
-  const categoryFilter = filters.find(filter => filter.id === "category");
-  const mostCommonCategoryPath = categoryFilter?.values.find(value => value.id === mostCommonCategoryId)?.path_from_root;
+  const categoryFilter = filters.find((filter) => filter.id === "category");
+  const mostCommonCategoryPath = categoryFilter?.values.find(
+    (value) => value.id === mostCommonCategoryId
+  )?.path_from_root;
   const defaultCategoryPath = categoryFilter?.values[0]?.path_from_root;
   return mostCommonCategoryPath || defaultCategoryPath || [];
 }
